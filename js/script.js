505df5cc6ce85aca1d1ac413964886c6baca9c05
BorislavUtkov <borislavutkov1@gmail.com>
2025-05-17 14:00:30 +0500

    Complete first lesson

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 "use strict";
 
-let n1 = 'borislavutkov';
-console.log(`https://vk.com/${n1}`);
+
+const personalMovieDB = {
+    count: +prompt("Сколько фильмов вы уже просмотрели?", "0"),
+    movies: {}, //
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+
+
+console.log(personalMovieDB);
+
